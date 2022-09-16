56e8467a0b3387d3cd252c29647a0e375e805d6f
William Dyson <wdyson@cloudera.com>
2022-09-16 16:41:07 +0100

    added simple json parse test

--- src/test/java/TestJson.java
+++ src/test/java/TestJson.java
@@ -1,8 +1,37 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+import com.cloudera.wdyson.flink.auditsession.Audit;
+
 import org.junit.jupiter.api.Test;
 
-class TestTests {
-    @Test
-    void succeedingTest() {
+class TestJson {
+    private String readResource(String path) throws IOException, URISyntaxException {
+        ClassLoader classLoader = getClass().getClassLoader();
 
+        return String.join("\n",
+            Files.readAllLines(Paths.get(classLoader.getResource(path).toURI())));
+    }
+
+    @Test
+    void simpleAuditParseTest() throws Exception {
+        String json = readResource("audit-examples/kafka-1.json");
+
+        Optional<Audit> maybeAudit = Audit.fromJson(json);
+
+        if (!maybeAudit.isPresent()) {
+            fail("Simple audit could not be parsed");
+        }
+
+        Audit audit = maybeAudit.get();
+
+        assertEquals(9, audit.repoType);
+        assertEquals("wdyson", audit.reqUser);
     }
 }
