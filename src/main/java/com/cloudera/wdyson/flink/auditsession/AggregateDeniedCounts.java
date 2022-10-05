5666bfa8d43d301e1d5e4ce3211f8f8bb11be064
William Dyson <wdyson@cloudera.com>
2022-10-05 17:27:29 +0100

    added comments to the AggregateDeniedCounts

--- src/main/java/com/cloudera/wdyson/flink/auditsession/AggregateDeniedCounts.java
+++ src/main/java/com/cloudera/wdyson/flink/auditsession/AggregateDeniedCounts.java
@@ -5,8 +5,10 @@ import org.apache.flink.api.common.functions.AggregateFunction;
 public class AggregateDeniedCounts implements AggregateFunction<Audit, Integer, Integer> {
     @Override
     public Integer add(Audit audit, Integer acc) {
+        // result == 1 means the action was allowed
         boolean allowed = audit.result == 1;
 
+        // if not allowed then increment the count by 1
         if (!allowed) {
             return acc + 1;
         } else {
@@ -16,6 +18,7 @@ public class AggregateDeniedCounts implements AggregateFunction<Audit, Integer, 
 
     @Override
     public Integer createAccumulator() {
+        // start count at 0
         return 0;
     }
 
